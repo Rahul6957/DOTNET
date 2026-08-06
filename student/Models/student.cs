c680ff99b575945be24c3227c083d3c8d08bf881
rahulgayke1704 <rahulgayke1704@gmail.com>
2026-08-06 22:19:34 +0530

    Rs

---
+++ student/Models/student.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace student.Models;
+
+public class Student{
+    public int id{get;set;}
+    public string name{get;set;}
+    public string school{get;set;}
+    public int std{get;set;}
+
+
+
+
+
+
+}
