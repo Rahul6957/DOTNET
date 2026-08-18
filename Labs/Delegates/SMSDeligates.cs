2976a8bd13b287fde7d8dffd79c1cc2209aca145
rahulgayke1704 <rahulgayke1704@gmail.com>
2026-08-18 20:39:38 +0530

    Delegates

---
+++ Labs/Delegates/SMSDeligates.cs
@@ -0,0 +1,19 @@
+namespace DEligates;
+
+class Delegate
+{
+    //one delegate
+   delegate void  SMSDelegate();
+
+   // five different methods
+   static void SendSms()
+    {
+        Console.WriteLine("sms send");
+
+    }
+static void sendOTP()
+    {
+        Console.
+    }
+
+}
