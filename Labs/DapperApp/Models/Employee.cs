adec02035eb365867df1cd706a8c75dcfe3782aa
rahulgayke1704 <rahulgayke1704@gmail.com>
2026-08-22 15:10:53 +0530

    new changes

--- Labs/DapperApp/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace DapperApp.Models;
-
-public class Employee
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Email { get; set; }
-    public decimal Salary { get; set; }
-}
